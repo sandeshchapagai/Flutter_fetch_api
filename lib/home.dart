8f4bfb06df3c06b23450f893158d4ae0cd189237
Sandy <sandeshchapagai@users.noreply.github.com>
2023-09-07 16:58:22 +0545

    nepali news api fetched

--- lib/home.dart
+++ lib/home.dart
@@ -1,62 +1,94 @@
+import 'dart:async';
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
-  const Home({super.key});
+  const Home({Key? key}) : super(key: key);
 
   @override
-  State<Home> createState() => _HomeState();
+  _HomeState createState() => _HomeState();
 }
+
 class _HomeState extends State<Home> {
+  List<String> userNames = [];
+  int currentIndex = 0;
+  Timer? _scrollTimer;
+
   @override
-  List<dynamic> users =[];
+  void initState() {
+    super.initState();
+    fetchUsers();
+  }
+
+  @override
+  void dispose() {
+    _scrollTimer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Api call'),
+        title: Text('API Call with Auto-Scrolling Text'),
         backgroundColor: Colors.blueGrey,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: fetchUsers,
       ),
       body: Container(
         color: Colors.white54,
-        child: ListView.builder(
-            itemCount: users.length,
-            itemBuilder: (context,index){
-          final  user=users[index];
-          final  name = user['name']['first'];
-          final email =user['email'];
-          final imageUrl=user['picture']['thumbnail'];
-          final phone=user['phone'];
-          return ListTile(
-            leading: ClipRRect(
-              borderRadius: BorderRadius.circular(100),
-              child:  Image.network(imageUrl)
-            ),
-            title: Text(name.toString()),
-            // subtitle: Text(email),
-            subtitle: Text(phone)
-
-          );
-        }),
+        child: SizedBox(
+          height: 40, // Set the desired height for the scrolling area
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: userNames.length * 2, // Double the length for looping effect
+            itemBuilder: (context, index) {
+              final userName = userNames[index % userNames.length];
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Text(
+                  userName,
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
+
   void fetchUsers() async {
-    print('Api Fetched');
-    const url = 'https://randomuser.me/api/?results=1000';
+    print('API Fetching...');
+    const url =
+        'https://newsdata.io/api/1/news?country=np&category=top&language=ne&apikey=pub_289961268fcc7c68fd65e762924c2c64a1a59';
     final uri = Uri.parse(url);
     final response = await http.get(uri);
-    final body = response.body;
-    final json = jsonDecode(body);
 
-    setState((){
-       users = json['results'];
+    if (response.statusCode == 200) {
+      final json = jsonDecode(utf8.decode(response.bodyBytes));
+      final results = json['results'] as List<dynamic>;
+
+      setState(() {
+        userNames = results.map((user) => user['title'].toString()).toList();
+      });
+
+      print('Completed');
+      startAutoScroll();
+    } else {
+      print('Failed to fetch data. Status code: ${response.statusCode}');
+    }
+  }
+
+  void startAutoScroll() {
+    const scrollDuration = Duration(seconds: 100); // Adjust the duration as needed
+
+    _scrollTimer = Timer.periodic(scrollDuration, (_) {
+      setState(() {
+        currentIndex++;
+        if (currentIndex >= userNames.length) {
+          currentIndex = 0;
+        }
+      });
     });
-    print('Completed');
   }
 }
